6294a6a4ac1a33ebf3d38693d57d770a00bed78c
hope <165012913+mhopsa@users.noreply.github.com>
2024-05-28 17:02:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-1cc1987a-1c05-4da1-912a-1b6cf303103f/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-62fb37fe-3782-4ae7-a191-f293b7f699c8/)
 
 
 echo "$curl_output" >> Update.tsx
